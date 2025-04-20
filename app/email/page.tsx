a5989f9135a34ea780f2d0d4b95878af204b67ad
Robiul Hasan <robiulhasan9559@gmail.com>
2025-04-20 22:09:03 +0600

    (Feature) Submenu link active

---
+++ app/email/page.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const emailPage = () => {
+    return (
+        <div>
+            <h1 className="text-2xl font-bold">This is Email Page</h1>
+        </div>
+    );
+};
+
+export default emailPage;
--- components/nav-main.tsx
+++ components/nav-main.tsx
@@ -19,6 +19,7 @@ import {
 } from "@/components/ui/sidebar"
 import Link from "next/link"
 import { usePathname } from "next/navigation"
+import { cn } from "@/lib/utils"
 
 type SidebarItem = {
   title?: string
@@ -44,10 +45,10 @@ export function NavMain({ items } : {items:SidebarItem[]}) {
       <SidebarMenu>
         {
           items.map((item) => {
-          // Check if this group should be expanded based on current path
-          const isGroupActive = item.items?.some((subItem) => pathname === subItem.url || pathname.startsWith(subItem.url))
-          console.log(`Pathname is: ${isGroupActive}`);
-          
+            // Check if this group should be expanded based on current path
+            const isGroupActive = item.items?.some((subItem) =>
+              pathname === subItem.url || pathname.startsWith(subItem.url)
+            )
 
             if(item.items && item.items.length > 0) {
               return (
@@ -59,24 +60,43 @@ export function NavMain({ items } : {items:SidebarItem[]}) {
                   >
                   <SidebarMenuItem className="">
                     <CollapsibleTrigger asChild>
-                      <SidebarMenuButton tooltip={item.title} className={`cursor-pointer py-5.5 px-3 text-base data-[state=open]:bg-primary hover:data-[state=open]:bg-primary data-[state=open]:text-white hover:data-[state=open]:text-white ${isGroupActive ? 'bg-red-600' : ''}`}>
+                      <SidebarMenuButton 
+                        tooltip={item.title}   
+                        className={cn(
+                          "cursor-pointer py-5.5 px-3 text-base hover:data-[state=open]:bg-primary hover:data-[state=open]:text-white",
+                          isGroupActive ? 'bg-primary text-white hover:bg-primary hover:text-white' : ''
+                        )}>
                         {item.icon && <item.icon />}
                         <span>{item.title}</span>
                         <ChevronRight className="ms-auto transition-transform duration-200 group-data-[state=open]/collapsible:rotate-90" />
                       </SidebarMenuButton>
                     </CollapsibleTrigger>
                     <CollapsibleContent>
-                      <SidebarMenuSub className="gap-0">
-                        {item.items?.map((subItem) => (
-                          <SidebarMenuSubItem key={subItem.title}>
-                            <SidebarMenuSubButton asChild className={`py-5.5 px-3 text-base`}>
-                              <Link href={subItem.url} className={`flex items-center gap-3.5 `}>
-                                <span className={`w-2 h-2 rounded-[50%] ${subItem.circleColor}`}></span>
-                                <span>{subItem.title}</span>
-                              </Link>
-                            </SidebarMenuSubButton>
-                          </SidebarMenuSubItem>
-                        ))}
+                      <SidebarMenuSub className="gap-0 mt-2">
+                        {item.items?.map((subItem) => 
+                          {
+                            const isSubActive = pathname === subItem.url || pathname.startsWith(subItem.url);
+                            return (
+                              <SidebarMenuSubItem key={subItem.title}>
+                                <SidebarMenuSubButton
+                                  asChild
+                                  className={cn(
+                                    "py-5.5 px-3 text-base hover:bg-[#e4f1ff]",
+                                    isSubActive
+                                      ? "bg-[#e4f1ff] font-bold"
+                                      : ""
+                                  )}
+                                >
+                                <Link href={subItem.url} className={`flex items-center gap-3.5`}>
+                                  <span className={`w-2 h-2 rounded-[50%] ${subItem.circleColor}`}></span>
+                                  <span>{subItem.title}</span>
+                                </Link>
+                              </SidebarMenuSubButton>
+                            </SidebarMenuSubItem>
+                            )
+                          }
+                         
+                        )}
                       </SidebarMenuSub>
                     </CollapsibleContent>
                   </SidebarMenuItem>
