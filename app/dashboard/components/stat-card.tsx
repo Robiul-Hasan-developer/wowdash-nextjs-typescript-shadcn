11f2c643d59ecc8ddff4e0004db4f3361a59d3e1
Robiul Hasan <robiulhasan9559@gmail.com>
2025-04-27 13:16:08 +0600

    Cards data dynamiced

--- app/dashboard/components/stat-card.tsx
+++ app/dashboard/components/stat-card.tsx
@@ -1,10 +1,92 @@
 import { UsersRound, ArrowUp, ArrowDown, Medal, Wallet, FileText } from 'lucide-react';
 import React from 'react';
 
+const cardsDatas = [
+    {
+      title: "Total Users",
+      value: "20,000",
+      icon: UsersRound,
+      iconBg: "bg-cyan-600",
+      gradientFrom: "from-cyan-600/10",
+      growth: "+4000",
+      growthIcon: ArrowUp,
+      growthColor: "text-green-600 dark:text-green-400",
+      description: "Last 30 days users",
+    },
+    {
+      title: "Total Subscription",
+      value: "15,000",
+      icon: Medal,
+      iconBg: "bg-purple-600",
+      gradientFrom: "from-purple-600/10",
+      growth: "-800",
+      growthIcon: ArrowDown,
+      growthColor: "text-red-600 dark:text-red-400",
+      description: "Last 30 days subscription",
+    },
+    {
+      title: "Total Free Users",
+      value: "5,000",
+      icon: UsersRound,
+      iconBg: "bg-blue-600",
+      gradientFrom: "from-blue-600/10",
+      growth: "+200",
+      growthIcon: ArrowUp,
+      growthColor: "text-green-600 dark:text-green-400",
+      description: "Last 30 days users",
+    },
+    {
+      title: "Total Income",
+      value: "$42,000",
+      icon: Wallet,
+      iconBg: "bg-green-600",
+      gradientFrom: "from-green-600/10",
+      growth: "+$20,000",
+      growthIcon: ArrowUp,
+      growthColor: "text-green-600 dark:text-green-400",
+      description: "Last 30 days income",
+    },
+    {
+      title: "Total Expense",
+      value: "$30,000",
+      icon: FileText,
+      iconBg: "bg-red-600",
+      gradientFrom: "from-red-600/10",
+      growth: "+$5,000",
+      growthIcon: ArrowUp,
+      growthColor: "text-green-600 dark:text-green-400",
+      description: "Last 30 days expense",
+    },
+]
+
 const StatCard = () => {
     return (
         <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-2 lg:grid-cols-3 2xl:grid-cols-5 gap-6">
-            <div className="card shadow-none border border-gray-200 dark:border-neutral-600 dark:bg-neutral-700 rounded-lg h-full bg-gradient-to-r from-cyan-600/10 to-bg-white">
+            {
+                cardsDatas.map((cardData, index) => (
+                    <div className={`card shadow-none border border-gray-200 dark:border-neutral-600 dark:bg-neutral-700 rounded-lg h-full bg-gradient-to-r ${cardData.gradientFrom} to-bg-white`} key={index}>
+                        <div className="card-body p-5">
+                            <div className="flex flex-wrap items-center justify-between gap-3">
+                                <div>
+                                    <p className="font-medium text-neutral-900 dark:text-white mb-1">{cardData.title}</p>
+                                    <h6 className="mb-0 dark:text-white">{cardData.value}</h6>
+                                </div>
+                                <div className={`w-[50px] h-[50px] ${cardData.iconBg} rounded-full flex justify-center items-center`}>
+                                    <cardData.icon className='text-white' />
+                                </div>
+                            </div>
+                            <p className="font-medium text-sm text-neutral-600 dark:text-white mt-3 mb-0 flex items-center gap-2">
+                            <span className={`inline-flex items-center gap-1 ${cardData.growthColor}`}>
+                                <cardData.growthIcon className='text-xs' width="14" height="14" />
+                                {cardData.growth}</span> 
+                                {cardData.description}
+                            </p>
+                        </div>
+                    </div>
+                ))
+            }
+            
+            {/* <div className="card shadow-none border border-gray-200 dark:border-neutral-600 dark:bg-neutral-700 rounded-lg h-full bg-gradient-to-r from-cyan-600/10 to-bg-white">
                 <div className="card-body p-5">
                     <div className="flex flex-wrap items-center justify-between gap-3">
                         <div>
@@ -96,7 +178,7 @@ const StatCard = () => {
                     Last 30 days expense
                     </p>
                 </div>
-            </div>
+            </div> */}
         </div>
     );
 };
