2b205c046abb2835906d501e9590ef1a1de9c7c7
Robiul Hasan <robiulhasan9559@gmail.com>
2025-04-30 13:43:01 +0600

    (Feature) Table component & style added

---
+++ components/table/data.ts
@@ -0,0 +1,100 @@
+// data/users.ts
+import UserImg1 from '../../public/assets/images/users/user1.png';
+import UserImg2 from '../../public/assets/images/users/user2.png';
+import UserImg3 from '../../public/assets/images/users/user3.png';
+import UserImg4 from '../../public/assets/images/users/user4.png';
+import UserImg5 from '../../public/assets/images/users/user5.png';
+
+export const users = [
+    {
+      name: "Dianne Russell",
+      email: "redaniel@gmail.com",
+      image: UserImg1,
+      registered: "27 Mar 2024",
+      plan: "Free",
+      statusBgWithColor: 'bg-green-100 dark:bg-green-600/25 text-green-600 dark:text-green-400',
+      status: "Active",
+    },
+    {
+      name: "Wade Warren",
+      email: "xterris@gmail.com",
+      image: UserImg2,
+      registered: "27 Mar 2024",
+      plan: "Basic",
+      statusBgWithColor: 'bg-red-100 dark:bg-red-600/25 text-red-600 dark:text-red-400',
+      status: "Inactive",
+    },
+    {
+      name: "Albert Flores",
+      email: "seannand@mail.ru",
+      image: UserImg3,
+      registered: "27 Mar 2024",
+      plan: "Standard",
+      statusBgWithColor: 'bg-green-100 dark:bg-green-600/25 text-green-600 dark:text-green-400',
+      status: "Active",
+    },
+    {
+      name: "Bessie Cooper",
+      email: "igerrin@gmail.com",
+      image: UserImg4,
+      registered: "27 Mar 2024",
+      plan: "Business",
+      statusBgWithColor: 'bg-red-100 dark:bg-red-600/25 text-red-600 dark:text-red-400',
+      status: "Active",
+    },
+    {
+      name: "Arlene McCoy",
+      email: "fellora@mail.ru",
+      image: UserImg5,
+      registered: "27 Mar 2024",
+      plan: "Enterprise",
+      statusBgWithColor: 'bg-green-100 dark:bg-green-600/25 text-green-600 dark:text-green-400',
+      status: "Active",
+    },
+    {
+        name: "Bessie Cooper",
+        email: "igerrin@gmail.com",
+        image: UserImg4,
+        registered: "27 Mar 2024",
+        plan: "Business",
+        statusBgWithColor: 'bg-red-100 dark:bg-red-600/25 text-red-600 dark:text-red-400',
+        status: "Active",
+      },
+      {
+        name: "Arlene McCoy",
+        email: "fellora@mail.ru",
+        image: UserImg5,
+        registered: "27 Mar 2024",
+        plan: "Enterprise",
+        statusBgWithColor: 'bg-green-100 dark:bg-green-600/25 text-green-600 dark:text-green-400',
+        status: "Active",
+      },
+      {
+        name: "Albert Flores",
+        email: "seannand@mail.ru",
+        image: UserImg3,
+        registered: "27 Mar 2024",
+        plan: "Standard",
+        statusBgWithColor: 'bg-green-100 dark:bg-green-600/25 text-green-600 dark:text-green-400',
+        status: "Active",
+      },
+      {
+        name: "Bessie Cooper",
+        email: "igerrin@gmail.com",
+        image: UserImg4,
+        registered: "27 Mar 2024",
+        plan: "Business",
+        statusBgWithColor: 'bg-red-100 dark:bg-red-600/25 text-red-600 dark:text-red-400',
+        status: "Active",
+      },
+      {
+        name: "Dianne Russell",
+        email: "redaniel@gmail.com",
+        image: UserImg1,
+        registered: "27 Mar 2024",
+        plan: "Free",
+        statusBgWithColor: 'bg-green-100 dark:bg-green-600/25 text-green-600 dark:text-green-400',
+        status: "Active",
+      },
+  ];
+  
