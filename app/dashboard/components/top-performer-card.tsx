471473bdb10ccd59809ec3ac06954cb72e4294f6
Robiul Hasan <robiulhasan9559@gmail.com>
2025-04-30 16:30:07 +0600

    (Feature) Common Link custom component added

--- app/dashboard/components/top-performer-card.tsx
+++ app/dashboard/components/top-performer-card.tsx
@@ -1,6 +1,5 @@
 import { Card, CardContent } from '@/components/ui/card';
 import React from 'react';
-import CustomSelect from '@/components/shared/custom-select';
 import Image, { StaticImageData } from "next/image";
 
 import PerformerImg1 from "../../../public/assets/images/users/user1.png";
@@ -9,8 +8,8 @@ import PerformerImg3 from "../../../public/assets/images/users/user3.png";
 import PerformerImg4 from "../../../public/assets/images/users/user4.png";
 import PerformerImg5 from "../../../public/assets/images/users/user5.png";
 import PerformerImg6 from "../../../public/assets/images/users/user1.png";
-import Link from 'next/link';
-import { ChevronRight } from 'lucide-react';
+import CommonLink from '@/components/shared/common-link';
+
 
 export interface TopPerformer {
     id: number;
@@ -71,10 +70,7 @@ const TopPerformerCard = () => {
             <CardContent className="card-body p-0">
                 <div className="flex items-center justify-between">
                     <h6 className="mb-3 font-semibold text-lg">Top Performer</h6>
-                    <Link href="#" className="flex items-center gap-1 text-blue-600 dark:text-blue-600 hover-text-blue-500 text-sm">
-                        View All
-                        <ChevronRight width={16} height={16} />
-                    </Link>
+                    <CommonLink />
                 </div>
 
                 <div className="mt-4 space-y-6">
@@ -98,65 +94,7 @@ const TopPerformerCard = () => {
                             )
                         })
                     }
-
-                    {/* <div className="flex items-center justify-between gap-2 mb-6">
-                        <div className="flex items-center gap-3">
-                            <img src="assets/images/users/user2.png" alt="" className="w-10 h-10 rounded-full shrink-0 overflow-hidden">
-                            <div className="grow">
-                            <h6 className="text-base mb-0 font-medium">Wade Warren</h6>
-                            <span className="text-sm text-secondary-light font-medium">Agent ID: 36254</span>
-                            </div>
-                        </div>
-                        <span className="text-neutral-600 dark:text-neutral-200 text-base font-medium">$20</span>
-                    </div>
-
-                    <div className="flex items-center justify-between gap-2 mb-6">
-                        <div className="flex items-center gap-3">
-                            <img src="assets/images/users/user3.png" alt="" className="w-10 h-10 rounded-full shrink-0 overflow-hidden">
-                            <div className="grow">
-                            <h6 className="text-base mb-0 font-medium">Albert Flores</h6>
-                            <span className="text-sm text-secondary-light font-medium">Agent ID: 36254</span>
-                            </div>
-                        </div>
-                        <span className="text-neutral-600 dark:text-neutral-200 text-base font-medium">$30</span>
-                    </div>
-
-                    <div className="flex items-center justify-between gap-2 mb-6">
-                        <div className="flex items-center gap-3">
-                            <img src="assets/images/users/user4.png" alt="" className="w-10 h-10 rounded-full shrink-0 overflow-hidden">
-                            <div className="grow">
-                            <h6 className="text-base mb-0 font-medium">Bessie Cooper</h6>
-                            <span className="text-sm text-secondary-light font-medium">Agent ID: 36254</span>
-                            </div>
-                        </div>
-                        <span className="text-neutral-600 dark:text-neutral-200 text-base font-medium">$40</span>
-                    </div>
-
-                    <div className="flex items-center justify-between gap-2 mb-6">
-                        <div className="flex items-center gap-3">
-                            <img src="assets/images/users/user5.png" alt="" className="w-10 h-10 rounded-full shrink-0 overflow-hidden">
-                            <div className="grow">
-                            <h6 className="text-base mb-0 font-medium">Arlene McCoy</h6>
-                            <span className="text-sm text-secondary-light font-medium">Agent ID: 36254</span>
-                            </div>
-                        </div>
-                        <span className="text-neutral-600 dark:text-neutral-200 text-base font-medium">$10</span>
-                    </div>
-
-                        <div className="flex items-center justify-between gap-2">
-                        <div className="flex items-center gap-3">
-                            <img src="assets/images/users/user1.png" alt="" className="w-10 h-10 rounded-full shrink-0 overflow-hidden">
-                            <div className="grow">
-                            <h6 className="text-base mb-0 font-medium">Arlene McCoy</h6>
-                            <span className="text-sm text-secondary-light font-medium">Agent ID: 36254</span>
-                            </div>
-                        </div>
-                        <span className="text-neutral-600 dark:text-neutral-200 text-base font-medium">$10</span>
-                    </div> */}
-
                 </div>
-
-            
             </CardContent>
         </Card>
     );
---
+++ components/shared/common-link.tsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import Link from 'next/link';
+import { ChevronRight } from 'lucide-react';
+
+interface LinkData {
+    href?: string;
+    text?: string
+}
+
+const CommonLink = ({ href = '#', text = "View All"  } : LinkData) => {
+    return (
+        <Link href={href} className="flex items-center gap-1 text-blue-600 dark:text-blue-600 hover-text-blue-500 text-sm">
+            {text}
+            <ChevronRight width={16} height={16} />
+        </Link>
+    );
+};
+
+export default CommonLink;
