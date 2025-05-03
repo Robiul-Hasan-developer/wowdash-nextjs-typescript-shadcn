da6d7e13cb3816898348fb747b959c86acd95d30
Robiul Hasan <robiulhasan9559@gmail.com>
2025-05-03 13:04:19 +0600

    (Feature) Country list data type added

--- components/shared/country-list.tsx
+++ components/shared/country-list.tsx
@@ -1,39 +1,55 @@
 import React from "react";
+import Image, { StaticImageData } from "next/image";
 
-export const countries = [
+import FlagImg1 from "../../public/assets/images/flags/flag1.png";
+import FlagImg2 from "../../public/assets/images/flags/flag2.png";
+import FlagImg3 from "../../public/assets/images/flags/flag3.png";
+import FlagImg4 from "../../public/assets/images/flags/flag4.png";
+import FlagImg5 from "../../public/assets/images/flags/flag5.png";
+
+export interface Country {
+  name: string;
+  users: string;
+  percent: number;
+  flag: StaticImageData;
+  color: string;
+}
+
+
+export const countries:Country[] = [
   {
     name: "USA",
     users: "1,240 Users",
     percent: 80,
-    flag: "/assets/images/flags/flag1.png",
+    flag: FlagImg1,
     color: "bg-blue-600",
   },
   {
     name: "Japan",
     users: "1,240 Users",
     percent: 60,
-    flag: "/assets/images/flags/flag2.png",
+    flag: FlagImg2,
     color: "bg-[#f86624]",
   },
   {
     name: "France",
     users: "1,240 Users",
     percent: 49,
-    flag: "/assets/images/flags/flag3.png",
+    flag: FlagImg3,
     color: "bg-yellow-500",
   },
   {
     name: "Germany",
     users: "1,240 Users",
     percent: 100,
-    flag: "/assets/images/flags/flag4.png",
+    flag: FlagImg4,
     color: "bg-green-600",
   },
   {
     name: "South Korea",
     users: "1,240 Users",
     percent: 30,
-    flag: "/assets/images/flags/flag5.png",
+    flag: FlagImg5,
     color: "bg-cyan-600",
   },
 ];
@@ -48,10 +64,10 @@ const CountryList = () => {
           className="flex items-center justify-between gap-3 mb-3 pb-2 last:mb-0 last:pb-0"
         >
           <div className="flex items-center">
-            <img
-              src={country.flag}
-              alt={country.name}
-              className="w-10 h-10 rounded-full shrink-0 me-4"
+            <Image
+                src={country.flag}
+                alt={country.name}
+                className="w-10 h-10 rounded-full shrink-0 me-4"
             />
             <div className="grow">
               <h6 className="text-sm mb-0">{country.name}</h6>
