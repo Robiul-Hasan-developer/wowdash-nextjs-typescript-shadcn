afe8838c9855a87bbad609de38bf5acbcac7d844
Robiul Hasan <robiulhasan9559@gmail.com>
2025-05-11 11:02:40 +0600

    (Added) Notice card added

--- app/investment/page.tsx
+++ app/investment/page.tsx
@@ -6,6 +6,7 @@ import StaticCard from "./components/static-card";
 import MostLocationCard from "./components/most-location-card";
 import MyPortfolioCard from "./components/my-portfolio-card";
 import LatestInvestmentsCard from "./components/latest-investments-card";
+import NoticeBoardCard from "./components/notice-board-card";
 
 const InvestmentPage = () => {
   return (
@@ -33,7 +34,7 @@ const InvestmentPage = () => {
           <LatestInvestmentsCard />
         </div>
         <div className="col-span-12 lg:col-span-6 2xl:col-span-4">
-          {/* <RevenueStatisticsCard /> */}
+          <NoticeBoardCard />
         </div>
         <div className="col-span-12 lg:col-span-6 2xl:col-span-4">
           {/* <RevenueStatisticsCard /> */}
--- components/table/latest-investments-table.tsx
+++ components/table/latest-investments-table.tsx
@@ -1,176 +1,5 @@
-// "use client";
-
-// import React from "react";
-// import Image, { StaticImageData } from "next/image";
-// import {
-//   Table,
-//   TableBody,
-//   TableCell,
-//   TableHead,
-//   TableHeader,
-//   TableRow,
-// } from "@/components/ui/table";
-
-// import UserImage1 from "../../public/assets/images/users/user1.png";
-// import UserImage2 from "../../public/assets/images/users/user2.png";
-// import UserImage3 from "../../public/assets/images/users/user3.png";
-// import UserImage4 from "../../public/assets/images/users/user4.png";
-// import UserImage5 from "../../public/assets/images/users/user5.png";
-
-// type AssetOrder = {
-//   userImage: string;
-//   userName: string;
-//   invoice: string;
-//   item: string;
-//   qty: string;
-//   amount: string;
-//   status: "Completed" | "In Progress";
-//   statusBg: string;
-// };
-
-// const orders: AssetOrder[] = [
-//   {
-//     userImage: "/assets/images/asset/asset-img1.png",
-//     userName: "Gold",
-//     invoice: "Main Asset",
-//     item: "Ounces",
-//     qty: "7400",
-//     amount: "$7,400.00",
-//     status: "Completed",
-//     statusBg:
-//       "bg-green-100 dark:bg-green-600/25 text-green-600 dark:text-green-400",
-//   },
-//   {
-//     userImage: "/assets/images/asset/asset-img2.png",
-//     userName: "Dollars",
-//     invoice: "Currency",
-//     item: "Dollars",
-//     qty: "5,40,000",
-//     amount: "$5,40,000.00",
-//     status: "In Progress",
-//     statusBg:
-//       "bg-yellow-100 dark:bg-yellow-500/25 text-yellow-500 dark:text-yellow-400",
-//   },
-//   {
-//     userImage: "/assets/images/asset/asset-img3.png",
-//     userName: "Stock Market",
-//     invoice: "Product",
-//     item: "Products",
-//     qty: "1400",
-//     amount: "$50,000.00",
-//     status: "Completed",
-//     statusBg:
-//       "bg-green-100 dark:bg-green-600/25 text-green-600 dark:text-green-400",
-//   },
-//   {
-//     userImage: "/assets/images/asset/asset-img4.png",
-//     userName: "Dimond",
-//     invoice: "Asset",
-//     item: "Ounces",
-//     qty: "350",
-//     amount: "$30,000.00",
-//     status: "In Progress",
-//     statusBg:
-//       "bg-yellow-100 dark:bg-yellow-500/25 text-yellow-500 dark:text-yellow-400",
-//   },
-//   {
-//     userImage: "/assets/images/asset/asset-img5.png",
-//     userName: "S&P 400",
-//     invoice: "Index",
-//     item: "Shares",
-//     qty: "24,000",
-//     amount: "$63,000.00",
-//     status: "Completed",
-//     statusBg:
-//       "bg-green-100 dark:bg-green-600/25 text-green-600 dark:text-green-400",
-//   },
-// ];
-
-// const LatestInvestmentsTable = () => {
-//   return (
-//     <Table className="table-auto border-spacing-0 border-separate">
-//       <TableHeader>
-//         <TableRow className="border-0">
-//           <TableHead className="bg-neutral-100 dark:bg-slate-700 text-base border-t border-neutral-200 dark:border-slate-600 px-4 h-12 border-s rounded-tl-lg">
-//             Asset
-//           </TableHead>
-//           <TableHead className="bg-neutral-100 dark:bg-slate-700 text-base border-t border-neutral-200 dark:border-slate-600 px-4 h-12">
-//             Quantity
-//           </TableHead>
-//           <TableHead className="bg-neutral-100 dark:bg-slate-700 text-base border-t border-neutral-200 dark:border-slate-600 px-4 h-12 text-center">
-//             Price
-//           </TableHead>
-//           <TableHead className="bg-neutral-100 dark:bg-slate-700 text-base border-t border-neutral-200 dark:border-slate-600 px-4 h-12 text-center">
-//             Date
-//           </TableHead>
-
-//           <TableHead className="bg-neutral-100 dark:bg-slate-700 text-base border-t border-neutral-200 dark:border-slate-600 px-4 h-12 border-e rounded-tr-lg text-center">
-//             Total Orders
-//           </TableHead>
-//         </TableRow>
-//       </TableHeader>
-
-//       <TableBody>
-//         {orders.map((order, index) => {
-//           const isLastRow = index === orders.length - 1;
-
-//           return (
-//             <TableRow key={index}>
-//               <TableCell
-//                 className={`py-3.5 px-4 first:border-s last:border-e border-b border-neutral-200 dark:border-slate-600 text-base first:border-s last:border-e ${
-//                   isLastRow ? "rounded-bl-lg" : ""
-//                 }`}
-//               >
-//                 <div className="flex items-center">
-//                   <Image
-//                     src={order.userImage}
-//                     alt={order.userName}
-//                     width={40}
-//                     height={40}
-//                     className="me-3 rounded-lg"
-//                   />
-//                   <span className="text-base font-semibold text-secondary-light">
-//                     {order.userName}
-//                   </span>
-//                 </div>
-//               </TableCell>
-
-//               <TableCell className="py-3.5 px-4 first:border-s last:border-e border-b border-neutral-200 dark:border-slate-600 text-base">
-//                 {order.invoice}
-//               </TableCell>
-
-//               <TableCell className="py-3.5 px-4 first:border-s last:border-e border-b border-neutral-200 dark:border-slate-600 text-base text-center">
-//                 {order.item}
-//               </TableCell>
-
-//               <TableCell className="py-3.5 px-4 first:border-s last:border-e border-b border-neutral-200 dark:border-slate-600 text-base text-center">
-//                 {order.qty}
-//               </TableCell>
-
-//               <TableCell
-//                 className={`py-3.5 px-4 first:border-s last:border-e border-b border-neutral-200 dark:border-slate-600 text-base text-center ${
-//                   isLastRow ? "rounded-br-lg" : ""
-//                 }`}
-//               >
-//                 <span
-//                   className={`px-4 py-1.5 rounded-full font-medium text-sm ${order.statusBg}`}
-//                 >
-//                   {order.status}
-//                 </span>
-//               </TableCell>
-//             </TableRow>
-//           );
-//         })}
-//       </TableBody>
-//     </Table>
-//   );
-// };
-
-// export default LatestInvestmentsTable;
-
 "use client";
 
-import Image from "next/image";
 import {
   Table,
   TableBody,
@@ -180,8 +9,15 @@ import {
   TableRow,
 } from "@/components/ui/table";
 
+import Image, { StaticImageData } from "next/image";
+import AssetImage1 from "../../public/assets/images/asset/asset-img1.png";
+import AssetImage2 from "../../public/assets/images/asset/asset-img2.png";
+import AssetImage3 from "../../public/assets/images/asset/asset-img3.png";
+import AssetImage4 from "../../public/assets/images/asset/asset-img4.png";
+import AssetImage5 from "../../public/assets/images/asset/asset-img5.png";
+
 type AssetOrder = {
-  image: string;
+  image: StaticImageData;
   name: string;
   subText: string;
   quantity: string;
@@ -194,7 +30,7 @@ type AssetOrder = {
 
 const orders: AssetOrder[] = [
   {
-    image: "/assets/images/asset/asset-img1.png",
+    image: AssetImage1,
     name: "Gold",
     subText: "Main Asset",
     quantity: "7400",
@@ -206,7 +42,7 @@ const orders: AssetOrder[] = [
       "bg-green-100 dark:bg-green-600/25 text-green-600 dark:text-green-400",
   },
   {
-    image: "/assets/images/asset/asset-img2.png",
+    image: AssetImage2,
     name: "Dollars",
     subText: "Currency",
     quantity: "5,40,000",
@@ -218,7 +54,7 @@ const orders: AssetOrder[] = [
       "bg-yellow-100 dark:bg-yellow-500/25 text-yellow-500 dark:text-yellow-400",
   },
   {
-    image: "/assets/images/asset/asset-img3.png",
+    image: AssetImage3,
     name: "Stock Market",
     subText: "Product",
     quantity: "1400",
@@ -230,7 +66,7 @@ const orders: AssetOrder[] = [
       "bg-green-100 dark:bg-green-600/25 text-green-600 dark:text-green-400",
   },
   {
-    image: "/assets/images/asset/asset-img4.png",
+    image: AssetImage4,
     name: "Dimond",
     subText: "Asset",
     quantity: "350",
@@ -242,7 +78,7 @@ const orders: AssetOrder[] = [
       "bg-yellow-100 dark:bg-yellow-500/25 text-yellow-500 dark:text-yellow-400",
   },
   {
-    image: "/assets/images/asset/asset-img5.png",
+    image: AssetImage5,
     name: "S&P 400",
     subText: "Index",
     quantity: "24,000",
