88068ef034a21b54c420ae74e107fbec1e9f0ab4
Robiul Hasan <robiulhasan9559@gmail.com>
2025-05-14 13:07:19 +0600

    (add) Github button & icon added

--- components/auth/social-login.tsx
+++ components/auth/social-login.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import FacebookIcon from "@/public/assets/images/icons/facebook-icon.png";
+import GithubIcon from "@/public/assets/images/icons/github-icon.png";
 import GoogleIcon from "@/public/assets/images/icons/google-icon.png";
 import Image from "next/image";
 import { Button } from "../ui/button";
@@ -9,17 +9,17 @@ const SocialLogin = () => {
     <>
       <form className="mt-8 flex items-center gap-3">
         <Button
-          className="font-semibold text-neutral-600 hover:text-neutral-600 dark:text-neutral-200 py-6 px-2 w-1/2 border border-neutral-600/30 rounded-xl text-sm flex items-center justify-center gap-3 line-height-1 hover:border-blue-300 hover:bg-blue-600/10"
+          className="font-semibold text-neutral-600 hover:text-neutral-600 dark:text-neutral-200 py-6 px-2 w-1/2 border border-neutral-600/30 rounded-xl text-sm flex items-center justify-center gap-3 line-height-1 hover:border-slate-400 hover:bg-slate-600/10"
           variant="outline"
           type="submit"
           name="action"
           value="facebook"
         >
-          <Image src={FacebookIcon} alt="Facebook" width={16} height={16} />
-          Facebook
+          <Image src={GithubIcon} alt="github" width={16} height={16} />
+          Github
         </Button>
         <Button
-          className="font-semibold text-neutral-600 hover:text-neutral-600 dark:text-neutral-200 py-6 px-2 w-1/2 border border-neutral-600/30 rounded-xl text-sm flex items-center justify-center gap-3 line-height-1 hover:border-blue-300 hover:bg-blue-600/10"
+          className="font-semibold text-neutral-600 hover:text-neutral-600 dark:text-neutral-200 py-6 px-2 w-1/2 border border-neutral-600/30 rounded-xl text-sm flex items-center justify-center gap-3 line-height-1 hover:border-blue-400 hover:bg-blue-600/10"
           variant="outline"
           type="submit"
           name="action"
