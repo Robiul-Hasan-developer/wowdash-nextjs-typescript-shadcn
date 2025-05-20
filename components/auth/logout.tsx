c5f3ac7beead97728599edc98f4db194d259ce42
Robiul Hasan <robiulhasan9559@gmail.com>
2025-05-20 09:12:01 +0600

    (Fix) Logout loading added when user logout

--- components/auth/logout.tsx
+++ components/auth/logout.tsx
@@ -4,7 +4,7 @@ import { useState } from "react";
 import { doLogout } from "@/app/actions";
 import { Button } from "../ui/button";
 import { useRouter } from "next/navigation";
-import { LogOutIcon } from "lucide-react";
+import { LogOutIcon, Loader2 } from "lucide-react";
 import toast from "react-hot-toast";
 
 const Logout = () => {
@@ -25,13 +25,22 @@ const Logout = () => {
     <Button
       onClick={handleLogout}
       type="button"
-      className={`hover:text-red-600 flex items-center gap-3 py-0.5 text-neutral-900 dark:text-white text-base !px-0.5 cursor-pointer leading-[0] w-full justify-start hover:no-underline h-[none] ${
+      className={`hover:text-red-600 flex items-center gap-3 py-0.5 text-neutral-900 dark:text-white text-base !px-0.5 cursor-pointer leading-[0] w-full justify-start hover:no-underline h-[25px] ${
         loading ? "text-red-600" : ""
       }`}
       variant="link"
     >
-      <LogOutIcon width={24} height={24} className="!w-4.5 !h-4.5" />
-      {loading ? "Logging out..." : "Logout"}
+      {loading ? (
+        <>
+          <Loader2 className="animate-spin !w-4.5 !h-4.5" />
+          Logging out...
+        </>
+      ) : (
+        <>
+          <LogOutIcon width={24} height={24} className="!w-4.5 !h-4.5" />
+          Logout
+        </>
+      )}
     </Button>
   );
 };
