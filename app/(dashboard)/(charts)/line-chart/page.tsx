c6eadcbdb9ba7ebdfdac1ee401636116891a7fd0
Robiul Hasan <robiulhasan9559@gmail.com>
2025-05-28 16:28:51 +0600

    (Chart Page) line chart added

--- app/(dashboard)/(charts)/line-chart/page.tsx
+++ app/(dashboard)/(charts)/line-chart/page.tsx
@@ -1,15 +1,19 @@
 import React from "react";
 import DashboardBreadcrumb from "@/components/layout/dashboard-breadcrumb";
 import DefaultCardComponent from "../../components/default-card-component";
+import LineChart from "@/components/charts/line-chart";
 
 const LineChartPage = () => {
     return (
         <>
             <DashboardBreadcrumb title="Line Chart" text="Line Chart" />
 
-            <div className="grid grid-cols-1 sm:grid-cols-2 gap-6">
+            <div className="grid grid-cols-1 md:grid-cols-2 gap-6">
                 <DefaultCardComponent title="Default Line Chart">
-                    CardContentGoesHere
+                    <LineChart />
+                </DefaultCardComponent>
+                <DefaultCardComponent title="Zoomable Chart">
+                    <LineChart />
                 </DefaultCardComponent>
             </div>
         </>
