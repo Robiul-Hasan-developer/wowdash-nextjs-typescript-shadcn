2200467e2b54dc4aa7089feab297118f3589319a
Robiul Hasan <robiulhasan9559@gmail.com>
2025-06-15 10:41:32 +0600

    (Feature) Edit profile tab of view profile page done

--- app/(dashboard)/view-profile/components/edit-profile-tab-content.tsx
+++ app/(dashboard)/view-profile/components/edit-profile-tab-content.tsx
@@ -2,6 +2,15 @@ import React from 'react';
 import { Label } from "@/components/ui/label";
 import { Input } from "@/components/ui/input";
 import AvatarUpload from './avatar-upload';
+import {
+    Select,
+    SelectContent,
+    SelectItem,
+    SelectTrigger,
+    SelectValue,
+} from "@/components/ui/select"
+import { Textarea } from '@/components/ui/textarea';
+import { Button } from '@/components/ui/button';
 
 const EditProfileTabContent = () => {
     return (
@@ -16,66 +25,80 @@ const EditProfileTabContent = () => {
                     <div className="col-span-12 sm:col-span-6">
                         <div className="mb-5">
                             <Label htmlFor="name" className="inline-block font-semibold text-neutral-600 dark:text-neutral-200 text-sm mb-2">Full Name <span className="text-red-600">*</span></Label>
-                            <Input type="text" className="form-control rounded-lg" id="name" placeholder="Enter Full Name" />
+                            <Input type="text" className="border border-neutral-300 px-5 dark:border-slate-500 focus:border-blue-600 dark:focus:border-blue-600 focus-visible:border-blue-600 h-12 rounded-lg !shadow-none !ring-0" id="name" placeholder="Enter Full Name" />
                         </div>
                     </div>
                     <div className="col-span-12 sm:col-span-6">
                         <div className="mb-5">
                             <Label htmlFor="email" className="inline-block font-semibold text-neutral-600 dark:text-neutral-200 text-sm mb-2">Email <span className="text-red-600">*</span></Label>
-                            <Input type="email" className="form-control rounded-lg" id="email" placeholder="Enter email address" />
+                            <Input type="email" className="border border-neutral-300 px-5 dark:border-slate-500 focus:border-blue-600 dark:focus:border-blue-600 focus-visible:border-blue-600 h-12 rounded-lg !shadow-none !ring-0" id="email" placeholder="Enter email address" />
                         </div>
                     </div>
                     <div className="col-span-12 sm:col-span-6">
                         <div className="mb-5">
                             <Label htmlFor="number" className="inline-block font-semibold text-neutral-600 dark:text-neutral-200 text-sm mb-2">Phone</Label>
-                            <Input type="email" className="form-control rounded-lg" id="number" placeholder="Enter phone number" />
+                            <Input type="email" className="border border-neutral-300 px-5 dark:border-slate-500 focus:border-blue-600 dark:focus:border-blue-600 focus-visible:border-blue-600 h-12 rounded-lg !shadow-none !ring-0" id="number" placeholder="Enter phone number" />
                         </div>
                     </div>
                     <div className="col-span-12 sm:col-span-6">
                         <div className="mb-5">
                             <Label htmlFor="depart" className="inline-block font-semibold text-neutral-600 dark:text-neutral-200 text-sm mb-2">Department <span className="text-red-600">*</span> </Label>
-                            <select className="form-control rounded-lg form-select" id="depart">
-                                <option>Enter Event Title </option>
-                                <option>Enter Event Title One </option>
-                                <option>Enter Event Title Two</option>
-                            </select>
+                            <Select>
+                                <SelectTrigger id='depart' className="border border-neutral-300 px-5 dark:border-slate-500 focus:border-blue-600 dark:focus:border-blue-600 focus-visible:border-blue-600 h-12 !rounded-lg !shadow-none !ring-0 !h-[48px] !w-full">
+                                    <SelectValue placeholder="Enter Department" />
+                                </SelectTrigger>
+                                <SelectContent>
+                                    <SelectItem value="English">English</SelectItem>
+                                    <SelectItem value="Bangla">Bangla</SelectItem>
+                                    <SelectItem value="Economics">Economics</SelectItem>
+                                </SelectContent>
+                            </Select>
                         </div>
                     </div>
                     <div className="col-span-12 sm:col-span-6">
                         <div className="mb-5">
                             <Label htmlFor="desig" className="inline-block font-semibold text-neutral-600 dark:text-neutral-200 text-sm mb-2">Designation <span className="text-red-600">*</span> </Label>
-                            <select className="form-control rounded-lg form-select" id="desig">
-                                <option>Enter Designation Title </option>
-                                <option>Enter Designation Title One </option>
-                                <option>Enter Designation Title Two</option>
-                            </select>
+                            <Select>
+                                <SelectTrigger id='desig' className="border border-neutral-300 px-5 dark:border-slate-500 focus:border-blue-600 dark:focus:border-blue-600 focus-visible:border-blue-600 h-12 !rounded-lg !shadow-none !ring-0 !h-[48px] !w-full">
+                                    <SelectValue placeholder="Enter Designation" />
+                                </SelectTrigger>
+                                <SelectContent>
+                                    <SelectItem value="Front End Dev"> Front End Dev </SelectItem>
+                                    <SelectItem value="Back End Dev"> Back End Dev </SelectItem>
+                                    <SelectItem value="UX/UI Designer"> UX/UI Designer </SelectItem>
+                                </SelectContent>
+                            </Select>
                         </div>
                     </div>
                     <div className="col-span-12 sm:col-span-6">
                         <div className="mb-5">
                             <Label htmlFor="Language" className="inline-block font-semibold text-neutral-600 dark:text-neutral-200 text-sm mb-2">Language <span className="text-red-600">*</span> </Label>
-                            <select className="form-control rounded-lg form-select" id="Language">
-                                <option> English</option>
-                                <option> Bangla </option>
-                                <option> Hindi</option>
-                                <option> Arabic</option>
-                            </select>
+                            <Select>
+                                <SelectTrigger id='Language' className="border border-neutral-300 px-5 dark:border-slate-500 focus:border-blue-600 dark:focus:border-blue-600 focus-visible:border-blue-600 h-12 !rounded-lg !shadow-none !ring-0 !h-[48px] !w-full">
+                                    <SelectValue placeholder="Enter Language" />
+                                </SelectTrigger>
+                                <SelectContent>
+                                    <SelectItem value="English">English</SelectItem>
+                                    <SelectItem value="Bangla">Bangla</SelectItem>
+                                    <SelectItem value="Economics">Economics</SelectItem>
+                                </SelectContent>
+                            </Select>
                         </div>
                     </div>
                     <div className="col-span-12">
                         <div className="mb-5">
                             <Label htmlFor="desc" className="inline-block font-semibold text-neutral-600 dark:text-neutral-200 text-sm mb-2">Description</Label>
-                            <textarea name="#0" className="form-control rounded-lg" id="desc" placeholder="Write description..."></textarea>
+                            <Textarea className="border border-neutral-300 px-5 dark:border-slate-500 focus:border-blue-600 dark:focus:border-blue-600 focus-visible:border-blue-600 h-12 !rounded-lg !shadow-none !ring-0 !h-[80px] !w-full" placeholder="Write Description" />
                         </div>
                     </div>
                 </div>
                 <div className="flex items-center justify-center gap-3">
-                    <button type="button" className="border border-red-600 bg-hover-red-200 text-red-600 text-base px-14 py-[11px] rounded-lg">
+                    <Button type="button" className="h-[48px] border border-red-600 bg-transparent hover:bg-red-600/20 dark:hover:bg-red-600/20 text-red-600 text-base px-14 py-[11px] rounded-lg">
                         Cancel
-                    </button>
-                    <button type="button" className="btn btn-primary border border-blue-600 text-base px-14 py-3 rounded-lg">
+                    </Button>
+                    <Button type="button" className="h-[48px] text-base px-14 py-3 rounded-lg">
                         Save
-                    </button>
+                    </Button>
                 </div>
             </form>
         </div>
--- app/(dashboard)/view-profile/components/view-profile-sidebar.tsx
+++ app/(dashboard)/view-profile/components/view-profile-sidebar.tsx
@@ -2,10 +2,10 @@ import React from 'react';
 
 const ViewProfileSidebar = () => {
     return (
-        <div className="user-grid-card relative border border-neutral-200 dark:border-neutral-600 rounded-2xl overflow-hidden bg-white dark:bg-neutral-700 h-full">
+        <div className="user-grid-card relative border border-slate-200 dark:border-slate-600 rounded-2xl overflow-hidden bg-white dark:bg-[#273142] h-full">
             <img src="assets/images/user-grid/user-grid-bg1.png" alt="" className="w-full object-fit-cover" />
             <div className="pb-6 ms-6 mb-6 me-6 -mt-[100px]">
-                <div className="text-center border-b border-neutral-200 dark:border-neutral-600">
+                <div className="text-center border-b border-slate-200 dark:border-slate-600">
                     <img src="assets/images/user-grid/user-grid-img14.png" alt="" className="border br-white border-width-2-px w-200-px h-[200px] rounded-full object-fit-cover mx-auto" />
                     <h6 className="mb-0 mt-4">Robiul Hasan</h6>
                     <span className="text-secondary-light mb-4">ifrandom@gmail.com</span>
@@ -39,7 +39,7 @@ const ViewProfileSidebar = () => {
                         </li>
                         <li className="flex items-center gap-1">
                             <span className="w-[30%] text-base font-semibold text-neutral-600 dark:text-neutral-200"> Bio</span>
-                            <span className="w-[70%] text-secondary-light font-medium">: Lorem Ipsum is simply dummy text of the       printing and typesetting industry.</span>
+                            <span className="w-[70%] text-secondary-light font-medium">: Lorem Ipsum is simply dummy text of the printing and typesetting industry.</span>
                         </li>
                     </ul>
                 </div>
