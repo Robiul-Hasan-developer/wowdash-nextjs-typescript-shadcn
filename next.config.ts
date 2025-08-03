e6c7edfb96ff5814364b8c6c2126a8a2945c68d9
Robiul Hasan <robiulhasan9559@gmail.com>
2025-08-03 12:44:55 +0600

    (Update) config.ts file updated

--- next.config.ts
+++ next.config.ts
@@ -2,20 +2,18 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   images: {
-    domains: ['lh3.googleusercontent.com', 'avatars.githubusercontent.com'],
-    
-    // remotePatterns: [
-    //   {
-    //     protocol: 'https',
-    //     hostname: 'lh3.googleusercontent.com',
-    //     pathname: '/**',
-    //   },
-    //   {
-    //     protocol: 'https',
-    //     hostname: 'avatars.githubusercontent.com',
-    //     pathname: '/**',
-    //   },
-    // ],
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'lh3.googleusercontent.com',
+        pathname: '/**',
+      },
+      {
+        protocol: 'https',
+        hostname: 'avatars.githubusercontent.com',
+        pathname: '/**',
+      },
+    ],
   },
 };
 
