831e03658258e535723d8957e4ff385bbff01525
Robiul Hasan <robiulhasan9559@gmail.com>
2025-08-23 20:36:36 +0600

    (Feature) Theme layout done

--- components/theme-customizer/svg-icons/helium-icon.tsx
+++ components/theme-customizer/svg-icons/helium-icon.tsx
@@ -1,8 +1,6 @@
 
-const HeliumIcon = () => {
+export const HeliumIcon = () => {
     return (
-        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 66 56" fill="none" aria-label="hydrogen" className="h-[92px] w-full"><path fill="currentColor" fillRule="evenodd" d="M58 1H8a7 7 0 0 0-7 7v40a7 7 0 0 0 7 7h50a7 7 0 0 0 7-7V8a7 7 0 0 0-7-7ZM8 0a8 8 0 0 0-8 8v40a8 8 0 0 0 8 8h50a8 8 0 0 0 8-8V8a8 8 0 0 0-8-8H8Z" clipRule="evenodd" opacity="0.1"></path><path fill="currentColor" d="M5 9a4 4 0 1 1 8 0 4 4 0 0 1-8 0Z"></path><path fill="currentColor" d="M32 9a5 5 0 0 1 5-5h20a5 5 0 0 1 5 5v38a5 5 0 0 1-5 5H37a5 5 0 0 1-5-5V9Zm-5-8h1v54h-1V1Z" opacity="0.1"></path><path fill="currentColor" d="M5 17.5A1.5 1.5 0 0 1 6.5 16h14a1.5 1.5 0 0 1 0 3h-14A1.5 1.5 0 0 1 5 17.5Z" opacity="0.7"></path><path fill="currentColor" d="M5 23.5A1.5 1.5 0 0 1 6.5 22h11a1.5 1.5 0 0 1 0 3h-11A1.5 1.5 0 0 1 5 23.5Z" opacity="0.4"></path></svg>
+        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 66 56" fill="none" aria-label="helium" className="h-[92px] w-full"><path fill="currentColor" fillRule="evenodd" d="M59 1H7a6 6 0 0 0-6 6v42a6 6 0 0 0 6 6h52a6 6 0 0 0 6-6V7a6 6 0 0 0-6-6ZM7 0a7 7 0 0 0-7 7v42a7 7 0 0 0 7 7h52a7 7 0 0 0 7-7V7a7 7 0 0 0-7-7H7Z" clipRule="evenodd" opacity="0.1"></path><path fill="currentColor" d="M28 9a5 5 0 0 1 5-5h24a5 5 0 0 1 5 5v38a5 5 0 0 1-5 5H33a5 5 0 0 1-5-5V9ZM4 9a5 5 0 0 1 5-5h9a5 5 0 0 1 5 5v38a5 5 0 0 1-5 5H9a5 5 0 0 1-5-5V9Z" opacity="0.1"></path><path fill="currentColor" d="M7 10a3 3 0 1 1 6 0 3 3 0 1 1-6 0Z"></path><path fill="currentColor" d="M7 17.5A1.5 1.5 0 0 1 8.5 16h10a1.5 1.5 0 0 1 0 3h-10A1.5 1.5 0 0 1 7 17.5Z" opacity="0.7"></path><path fill="currentColor" d="M7 23.5A1.5 1.5 0 0 1 8.5 22h8a1.5 1.5 0 0 1 0 3h-8A1.5 1.5 0 0 1 7 23.5Z" opacity="0.4"></path></svg>
     );
 };
-
-export default HeliumIcon;
--- components/theme-customizer/theme-components/theme-layout.tsx
+++ components/theme-customizer/theme-components/theme-layout.tsx
@@ -1,16 +1,40 @@
-import { useState } from "react";
-import HeliumIcon from "../svg-icons/helium-icon";
+import { useEffect, useState } from "react";
+import { BoronIcon } from "../svg-icons/boron-icon";
+import { HeliumIcon } from "../svg-icons/helium-icon";
+import { HydrogenIcon } from "../svg-icons/hydrogen-icon";
 
-const layouts = ["hydrogen", "helium", "lithium", "beryllium", "boron", "carbon"];
+const layouts = [
+    {
+        text: "hydrogen",
+        icon: <HydrogenIcon />,
+    },
+    {
+        text: "helium",
+        icon: <HeliumIcon />,
+    },
+    {
+        text: "boron",
+        icon: <BoronIcon />,
+    },
+];
 
 const ThemeLayout = () => {
-
     const [layout, setLayout] = useState("hydrogen");
 
+    // Load saved layout from localStorage on mount
+    useEffect(() => {
+        const savedLayout = localStorage.getItem("layout");
+        if (savedLayout) {
+            setLayout(savedLayout);
+            document.documentElement.setAttribute("layout", savedLayout);
+        }
+    }, []);
 
-    const handleLayoutClick = (selectedLayout: any) => {
+    // Handle user selecting a layout
+    const handleLayoutClick = (selectedLayout: string) => {
         setLayout(selectedLayout);
         document.documentElement.setAttribute("layout", selectedLayout);
+        localStorage.setItem("layout", selectedLayout);
     };
 
     return (
@@ -21,16 +45,23 @@ const ThemeLayout = () => {
                 {layouts.map((item, index) => (
                     <div className="" key={index}>
                         <div
-                            className={`cursor-pointer border border-neutral-300 flex items-center justify-center hover:bg-primary/10 duration-300 h-[92px] rounded-md ring-2 dark:border-slate-500 ${layout === item ? "ring-primary text-primary dark:text-primary" : "ring-transparent text-neutral-500 dark:text-white"}`}
-                            onClick={() => handleLayoutClick(item)}
+                            className={`cursor-pointer border border-neutral-300 flex items-center justify-center hover:bg-primary/10 duration-300 h-[92px] rounded-md ring-2 dark:border-slate-500 ${layout === item.text
+                                ? "ring-primary text-primary dark:text-primary"
+                                : "ring-transparent text-neutral-500 dark:text-white"
+                                }`}
+                            onClick={() => handleLayoutClick(item.text)}
                         >
-                            {/* <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 66 56" fill="none" aria-label="hydrogen" className="h-[92px] w-full"><path fill="currentColor" fillRule="evenodd" d="M58 1H8a7 7 0 0 0-7 7v40a7 7 0 0 0 7 7h50a7 7 0 0 0 7-7V8a7 7 0 0 0-7-7ZM8 0a8 8 0 0 0-8 8v40a8 8 0 0 0 8 8h50a8 8 0 0 0 8-8V8a8 8 0 0 0-8-8H8Z" clipRule="evenodd" opacity="0.1"></path><path fill="currentColor" d="M5 9a4 4 0 1 1 8 0 4 4 0 0 1-8 0Z"></path><path fill="currentColor" d="M32 9a5 5 0 0 1 5-5h20a5 5 0 0 1 5 5v38a5 5 0 0 1-5 5H37a5 5 0 0 1-5-5V9Zm-5-8h1v54h-1V1Z" opacity="0.1"></path><path fill="currentColor" d="M5 17.5A1.5 1.5 0 0 1 6.5 16h14a1.5 1.5 0 0 1 0 3h-14A1.5 1.5 0 0 1 5 17.5Z" opacity="0.7"></path><path fill="currentColor" d="M5 23.5A1.5 1.5 0 0 1 6.5 22h11a1.5 1.5 0 0 1 0 3h-11A1.5 1.5 0 0 1 5 23.5Z" opacity="0.4"></path></svg> */}
-                            <HeliumIcon />
+                            {item.icon}
                         </div>
                         <h6
-                            className={`text-center capitalize text-sm font-medium pt-2 cursor-pointer hover:text-blue-500 ${layout === item ? "text-primary dark:text-primary" : "text-neutral-500 dark:text-neutral-200"}`}
-                            onClick={() => handleLayoutClick(item)}
-                        >{item}</h6>
+                            className={`text-center capitalize text-sm font-medium pt-2 cursor-pointer hover:text-blue-500 ${layout === item.text
+                                ? "text-primary dark:text-primary"
+                                : "text-neutral-500 dark:text-neutral-200"
+                                }`}
+                            onClick={() => handleLayoutClick(item.text)}
+                        >
+                            {item.text}
+                        </h6>
                     </div>
                 ))}
             </div>
