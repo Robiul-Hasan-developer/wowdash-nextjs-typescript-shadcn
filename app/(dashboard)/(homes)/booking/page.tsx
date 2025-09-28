6a2f3c04ef646cabcf14cd4ca669e4c67a1eb8bf
Robiul Hasan <robiulhasan9559@gmail.com>
2025-09-28 11:36:30 +0600

    (Booking) available & statistics card added

---
+++ app/(dashboard)/(homes)/booking/page.tsx
@@ -0,0 +1,60 @@
+import DashboardBreadcrumb from "@/components/layout/dashboard-breadcrumb";
+import LoadingSkeleton from "@/components/loading-skeleton";
+import type { Metadata } from "next";
+import { Suspense } from "react";
+import AvailableRoom from "./components/available-room";
+import StatisticsCards from "./components/statistics-cards";
+
+const metadata: Metadata = {
+    title: "Booking System Dashboard | WowDash Admin Panel",
+    description:
+        "Manage reservations, track bookings, and streamline scheduling with the Booking System Dashboard in WowDash Admin Template built using Next.js and Tailwind.",
+};
+
+const FinancePage = () => {
+    return (
+        <>
+            <DashboardBreadcrumb title="Booking System" text="Booking System" />
+
+            <div className="mb-6">
+                <div className="grid grid-cols-1 sm:grid-cols-12 gap-6">
+
+                    <div className="col-span-12 md:col-span-6 2xl:col-span-4">
+                        <Suspense fallback={<LoadingSkeleton height="h-64" text="Loading..." />}>
+                            <StatisticsCards />
+                        </Suspense>
+                    </div>
+
+                    <div className="col-span-12 md:col-span-6 2xl:col-span-4">
+                        <Suspense fallback={<LoadingSkeleton height="h-64" text="Loading..." />}>
+                            <AvailableRoom />
+                        </Suspense>
+                    </div>
+
+                    <div className="col-span-12 2xl:col-span-4">
+                        <div className="shadow-7 p-5 rounded-xl bg-white dark:bg-dark-2 h-full">
+                            <div className="flex items-center flex-wrap gap-2 justify-between">
+                                <h6 className="mb-0 font-bold text-lg">Booking Statistic</h6>
+                                <div className="">
+                                    <select
+                                        className="form-select form-select-sm w-auto bg-white dark:bg-dark-2 border text-neutral-600 dark:text-white rounded-lg">
+                                        <option>Yearly</option>
+                                        <option>Monthly</option>
+                                        <option>Weekly</option>
+                                        <option>Today</option>
+                                    </select>
+                                </div>
+                            </div>
+                            <div className="relative">
+                                <div id="statisticBarChart" className="text-style label-bold"></div>
+                            </div>
+                        </div>
+                    </div>
+
+                </div>
+            </div>
+
+        </>
+    );
+};
+export default FinancePage;
