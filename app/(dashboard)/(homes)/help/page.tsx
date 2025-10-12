de0014d707ca7498bd29318722543c634421c736
Robiul Hasan <robiulhasan9559@gmail.com>
2025-10-12 12:43:41 +0600

    (Component) Top podcasters card added

--- app/(dashboard)/(homes)/help/page.tsx
+++ app/(dashboard)/(homes)/help/page.tsx
@@ -5,6 +5,7 @@ import type { Metadata } from "next";
 import { Suspense } from "react";
 import PendingSolvedTicketsCard from "./components/pending-solved-tickets-card";
 import TicketStatusCard from "./components/ticket-status-card";
+import TopPodcasterCard from "./components/top-podcaster-card";
 
 const metadata: Metadata = {
     title: "Booking System Dashboard | WowDash Admin Panel",
@@ -57,7 +58,7 @@ const HelpPage = () => {
 
                 <div className="col-span-12 lg:col-span-4">
                     <Suspense fallback={<LoadingSkeleton height="h-64" text="Loading..." />}>
-                        Hello
+                        <TopPodcasterCard />
                     </Suspense>
                 </div>
 
@@ -72,59 +73,7 @@ const HelpPage = () => {
                         Hello
                     </Suspense>
                 </div>
-
             </div>
-
-            {/* <div className="mb-6">
-                <div className="grid grid-cols-1 sm:grid-cols-12 gap-6">
-
-                    <div className="col-span-12 md:col-span-6 2xl:col-span-4">
-                        <Suspense fallback={<LoadingSkeleton height="h-64" text="Loading..." />}>
-                            <StatisticsCards />
-                        </Suspense>
-                    </div>
-
-                    <div className="col-span-12 md:col-span-6 2xl:col-span-4">
-                        <Suspense fallback={<LoadingSkeleton height="h-64" text="Loading..." />}>
-                            <AvailableRoom />
-                        </Suspense>
-                    </div>
-
-                    <div className="col-span-12 2xl:col-span-4">
-                        <div className="shadow-7 p-5 rounded-xl bg-white dark:bg-[#273142] h-full">
-                            <div className="flex items-center flex-wrap gap-2 justify-between">
-                                <h6 className="mb-0 font-bold text-lg">Booking Statistic</h6>
-                                <CustomSelect
-                                    placeholder="Monthly"
-                                    options={["Monthly", "Weekly", "Yearly",]}
-                                />
-                            </div>
-                            <div className="relative text-style label-bold">
-                                <StatisticBarChart />
-                            </div>
-                        </div>
-                    </div>
-
-                </div>
-            </div>
-
-            <div className="grid grid-cols-1 sm:grid-cols-12 gap-6">
-                <div className="col-span-12 md:col-span-6 xl:col-span-8">
-                    <div className="flex flex-col gap-6">
-                        <ExclusiveTravelPackages />
-                        <EarningStatisticCard />
-                        <TransactionHistory />
-                    </div>
-                </div>
-                <div className="col-span-12 md:col-span-6 xl:col-span-4">
-                    <div className="flex flex-col gap-6">
-                        <BookingCountriesStatus />
-                        <CheckInOut />
-                        <SpendOverview />
-                    </div>
-                </div>
-            </div> */}
-
         </>
     );
 };
